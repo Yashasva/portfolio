ce7d7e04cf23adeea39a1e9046f57f6806a7b291
Yashasva Paras <yashasvap@gmail.com>
2021-08-12 11:34:45 +0530

    added social media handles

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,15 +1,26 @@
 
 import profileImage from './GenosTwoFaceMeSmallVer.jpg';
+import linkedin from './linkedin.png';
+import github from './github.png';
+import twitter from './twitter.png';
+
 const Profile = ()=>{
     return(
-        <div className="home">
-            <div className="home-image">
-                <img src={profileImage} alt="Something's supposed to be here"/>
+        <div>
+            <div className="home">
+                <div className="home-image">
+                    <img src={profileImage} alt="Something's supposed to be here"/>
+                </div>
+                <div className="home-content">
+                    <p> Here, I have tried to put everything I do and I am interested in at one place. Feel free to look around.
+                    <br/> Connect with me? I love to connect with new people and exchange ideas. Email me at <a href = "mailto: yashasvap@gmail.com">yashasvap@gmail.com</a>
+                    </p>
+                </div>
             </div>
-            <div className="home-content">
-                <p> Here, I have tried to put everything I do and I am interested in at one place. Feel free to look around.
-                  <br/> Connect with me? I love to connect with new people and exchange ideas. Email me at <a href = "mailto: yashasvap@gmail.com">yashasvap@gmail.com</a>
-                </p>
+            <div className="social">
+                <div className="icon"><a href="https://www.linkedin.com/in/yashasva-paras-ab8833144/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="Linkedin"/></a></div>
+                <div className="icon"><a href="https://github.com/Yashasva" target="_blank" rel="noopener noreferrer"><img src={github} alt="github"/></a></div>
+                <div className="icon"><a href="https://twitter.com/YashasvaP" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter"/></a></div>
             </div>
         </div>
     )
