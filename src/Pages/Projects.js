19153bd2bd5908fa08e91ca4e25434575d86f7c0
Ranpo_Edogawa <31616629+Yashasva@users.noreply.github.com>
2021-11-01 01:14:33 +0530

    added project

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,9 +1,11 @@
 import Project from '../Components/Project/Project';
 import gipher from '../Components/Project/gipherapp.JPG';
 import todo from '../Components/Project/todo.JPG';
+import bookingticket from '../Components/Project/booking-ticket.JPG'
 
 const gipherText = "This application uses 3rd party API provided by giphy.com to search for Gifs according to the users need. The application also allows users to save their favorite gifs and show them all at one place. Front end has been built on Angular and back-end uses two microservices built on Spring Boot.";
 const todoText = "This application uses vanilla JS to implement a to-do list tracker. Users can add, edit, delete and mark them as completed/not completed.";
+const bookingText = "This application is build using React JS and Nodejs. User can book tickets in a given compartment. A user can book only seven tickets at a time however the number of bookings is not limited";
 
 const gipherdemo = "https://gipher-app.herokuapp.com/home";
 const giphergit = "https://github.com/Yashasva/GipherApp";
@@ -11,10 +13,14 @@ const giphergit = "https://github.com/Yashasva/GipherApp";
 const tododemo = "https://yashasva.github.io/Todo-List/";
 const todogit = "https://github.com/Yashasva/Todo-List";
 
+const bookingdemo = "https://brave-mcnulty-944ce7.netlify.app/";
+const bookinggit = "https://github.com/Yashasva/booking-system-frontend";
+
 function Projects(){
 return(
     <div>
         <Project title='Gipher App' url={gipher} desc={gipherText} demo={gipherdemo} github={giphergit}/>
+        <Project title='Booking System' url={bookingticket} desc={bookingText} demo={bookingdemo} github={bookinggit}/>
         <Project title='To Do App' url={todo} desc={todoText} demo={tododemo} github={todogit}/>
     </div>
 );
